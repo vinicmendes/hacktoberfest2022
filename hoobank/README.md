fc2e013f55cfd4fbc140101a7dcd02e4390ce68b
lucianobajr <luciano.alcantara@ufv.br>
2022-10-06 12:52:22 -0300

    add(+): hoobank tailwindcss

---
+++ hoobank/README.md
@@ -0,0 +1,3 @@
+# Hoobank
+
+- [Tutorial](https://www.youtube.com/watch?v=_oO4Qi5aVZs&ab_channel=JavaScriptMastery)
---
+++ hoobank/src/components/Testimonials.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Testimonials: React.FC = () => {
+  return <div />;
+};
+
+export default Testimonials;
